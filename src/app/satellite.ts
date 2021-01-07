b1dce14e6ce43ad782f3c52d8f94249dd384bc9e
AbudeyeW <AbudeyeW@gmail.com>
2021-01-06 18:34:12 -0600

    Done..?

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,7 +4,16 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
-    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
-        
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+        this.name = name;
+        this.type = type;
+        this.operational = operational;
+        this.orbitType = orbitType;
+        this.launchDate = launchDate;
+    }
+
+    shouldShowWarning(): boolean {
+        return this.type.toUpperCase() === "SPACE DEBRIS";
     }
 }
